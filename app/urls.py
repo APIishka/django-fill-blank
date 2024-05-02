4425ab5cd3df5c238e3c3b9e7b990468e60d06a5
APIishka <artem.plishka@gmail.com>
2024-05-02 05:56:05 -0700

    final working v, added answer submitting, table, results and some ui improvments

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('question/<int:test_id>/<int:question_id>/', views.question_detail, name='question_detail'),
-  path('submit_response/<int:test_id>/<int:question_id>/', views.submit_user_response, name='submit_response'),
+    path('', views.index, name='index'),
+    path('question/<int:test_id>/<int:question_id>/', views.question_detail, name='question_detail'),
+    path('submit_response/<int:test_id>/<int:question_id>/', views.submit_user_response, name='submit_response'),
+    path('submit_results/', views.submit_results, name='submit_results'),
+    path('delete_user_responses/', views.delete_user_responses, name='delete_user_responses'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import JsonResponse
-from .forms import AnswerForm
-from .models import Answer, Question, Test, UserResponse
+from .models import Question, Test, UserResponse
+from django.db.models import F
 
 def _format_question_text(text, placeholder):
     # Replace 'coh7$' with input field
@@ -32,7 +32,7 @@ def question_detail(request, test_id, question_id):
 def submit_user_response(request, test_id, question_id):
     if request.method == 'POST':
         answer_text = request.POST.get('answer', '')  # Get the answer text from the form
-        if answer_text:  # Check if the answer text is not empty
+        if answer_text:
             try:
                 test = Test.objects.get(pk=test_id)
                 question = Question.objects.get(pk=question_id)
@@ -44,11 +44,30 @@ def submit_user_response(request, test_id, question_id):
                     # Redirect to the next question
                     return redirect('question_detail', test_id=test_id, question_id=next_question.id)
                 else:
-                    # No more questions, redirect to the index page
-                    return redirect('index')
+                    # No more questions, redirect to the results page
+                    return redirect('submit_results')
             except (Test.DoesNotExist, Question.DoesNotExist) as e:
                 return JsonResponse({'error': str(e)}, status=404)  # Handle missing test or question
         else:
             return JsonResponse({'error': 'Answer text is required'}, status=400)  # Handle empty answer text
     else:
         return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)  # Handle non-POST requests
+
+
+def submit_results(request):
+    if request.method == 'GET':
+        user_responses = UserResponse.objects.all()
+        total_questions = Question.objects.count()
+        correct_answers = UserResponse.objects.filter(answer_text=F('question__answer__text')).count()
+        percentage_correct = (correct_answers / total_questions) * 100 if total_questions > 0 else 0
+        context = {
+            'user_responses': user_responses,
+            'percentage_correct': percentage_correct
+        }
+        return render(request, 'submit_results.html', context)
+    else:
+        return JsonResponse({'error': 'Only GET requests are allowed'}, status=405)
+    
+def delete_user_responses(request):
+    UserResponse.objects.all().delete()
+    return JsonResponse({'message': 'User responses deleted successfully'})
